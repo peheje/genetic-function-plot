5cbb9b2db84688605512659079fd3df86566f586
Peter Helstrup Jensen <peterhj@gmail.com>
2017-04-28 19:52:10 +0200

    clickable graph creates dataset

--- src/ui.js
+++ src/ui.js
@@ -3,6 +3,10 @@ const rd = require('./readDOM'), gi = rd.getInput, gin = rd.getInputNumber, gij 
 const gen = require('./genetics_poly');
 const random = require('./random');
 
+let mouseDown = false;
+let mouseCursor = { x: null, y: null };
+let mouseInGraph = false;
+
 function activate(initLoop, mainLoop) {
   // Run listener
   document.getElementById("run").addEventListener("click", (e) => {
@@ -33,6 +37,20 @@ function activate(initLoop, mainLoop) {
   document.getElementById("templates").addEventListener("change", e => {
     updateTemplate();
   });
+
+  // Click listeners
+  document.body.onmousedown = function () {
+
+    function round(x, dec) {
+      return Number(x.toFixed(dec));
+    }
+
+    console.log("mouse down");
+    if (!mouseInGraph || mouseCursor.x === null || mouseCursor.y === null) return;
+    let data = gij("data") || [];
+    data.push([round(mouseCursor.x, 2), round(mouseCursor.y, 2)]);
+    si("data", JSON.stringify(data));
+  }
 }
 
 function draw(best, i) {
@@ -40,9 +58,10 @@ function draw(best, i) {
     return;
   }
   let eq = gen.coefficientsToEquation(best);
+  si("eq1", eq);
 
   si("counter", i);
-  si("eq1", eq);
+
   try {
     let instance = functionPlot({
       xAxis: {
@@ -68,6 +87,17 @@ function draw(best, i) {
         graphType: "scatter",
         points: JSON.parse(gi("data"))
       }]
+    });
+    instance.on("mouseover", () => {
+      console.log("mouseover");
+      mouseInGraph = true;
+    });
+    instance.on("mouseout", () => {
+      console.log("mouseout");
+      mouseInGraph = false;
+    });
+    instance.on("mousemove", pos => {
+      mouseCursor = pos;
     });
   }
   catch (err) {
